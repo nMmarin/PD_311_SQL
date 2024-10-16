aea1311442831928089858b349291ee5cbb39aac
Marin_vc47@top-academy.ru <Marin_vc47@top-academy.ru>
2024-10-16 12:41:32 +0500

    Add project files.

---
+++ 01-DDL/DataBaseCreation/SQLQuery1-CreateDataBase.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE PD_311_DDL  
+ON 
+(
+	NAME = N'PD_311_DDL_dat',       
+	FILENAME = N'B:\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\PD_311_DDL.mdf',     
+	SIZE = 8MB,      
+	MAXSIZE = 512MB,       
+	FILEGROWTH = 8MB
+)  
+LOG ON 
+(
+	NAME = N'PD_311_DDL_log',         
+	FILENAME = N'B:\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\PD_311_DDL.ldf',
+	SIZE = 8MB,      
+	MAXSIZE = 512MB,       
+	FILEGROWTH = 8MB
+);
