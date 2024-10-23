874df31e1765f9b804b40b8296047276954afb45
Marin_vc47@top-academy.ru <Marin_vc47@top-academy.ru>
2024-10-23 17:34:41 +0500

    INSER Scedule 1 DOne

--- 01-DDL/Schedule/SQLQuery1.sql
+++ 01-DDL/Schedule/SQLQuery1.sql
@@ -14,18 +14,46 @@ PRINT (@teacher)
 PRINT (@number_of_lessons)
 
 DECLARE @lesson AS SMALLINT = 0;
-WHILE @lesson<@number_of_lessons
-BEGIN
-PRINT(FORMATMESSAGE(N'%s %s','Дата:', CAST(@date AS NVARCHAR(50))))
-PRINT(FORMATMESSAGE(N'%s %s','Вермя:',CAST(@time AS NVARCHAR(50))))
-PRINT(FORMATMESSAGE(N'%s %i','Урок №:',@lesson));
-SET @lesson=@lesson+1;
-PRINT ('-----------------------------------------------')
+--WHILE @lesson < @number_of_lessons
+--BEGIN
+--	--PRINT(FORMATMESSAGE(N'%s %s','Дата:', CAST(@date AS NVARCHAR(50))))
+--	--PRINT(FORMATMESSAGE(N'%s %s','День:', DATENAME(WEEKDAY,@date)));
+--	--PRINT(FORMATMESSAGE(N'%s %s','Вермя:',CAST(@time AS NVARCHAR(50))))
+--	--PRINT(FORMATMESSAGE(N'%s %i','Урок №:',@lesson));
+--		INSERT 
+--		Schedule	([date],[time],[group],disciplne,teacher,spent)
+--		VALUES		(@date,@time,@group,@discipline,@teacher,IIF(@date<GETDATE(),1,0))
+--	SET @lesson=@lesson+1;
+--	IF @lesson < @number_of_lessons
+--	BEGIN
+--		--PRINT(FORMATMESSAGE(N'%s %s','Дата:', CAST(@date AS NVARCHAR(50))));
+--		--PRINT(FORMATMESSAGE(N'%s %s','Вермя:', CAST(DATEADD(MINUTE,90,@time)AS NVARCHAR(50))));
+--		--PRINT(FORMATMESSAGE(N'%s %i','Урок №:',@lesson));
+--			INSERT 
+--		Schedule	([date],[time],						[group],	disciplne,	teacher,	spent)
+--		VALUES		(@date, DATEADD(MINUTE, 90, @time), @group,		@discipline,@teacher,	IIF(@date < GETDATE(),1,0))
+--		SET @lesson=@lesson+1;
+--	END
+--	SET @date= DATEADD(DAY,IIF(DATEPART(WEEKDAY, @date)=6,3,2),@date);
+--PRINT ('-----------------------------------------------')
+
+--END
+GO
+SELECT 
+	[Дата]=[date],
+	[Время]=[time],
+	[День]=DATENAME(WEEKDAY,[date]),
+	[Группа]=group_name,
+	[Дисциплина]=discipline_name,
+	[Преподователь]=FORMATMESSAGE(N'%s %s %s',last_name,first_name,middle_name),
+	[Стату проведения]= IIF (spent=1, N'Проведено',N'Запланировано')
+FROM		Schedule,Groups,Disciplines,Teachers
+WHERE		[group] = group_id
+AND			disciplne = discipline_id
+AND			teacher = teacher_id
 --INSERT 
 --Schedule	([date],[time],[group],disciplne,teacher,spent)
 --VALUES		('2024-10-21','13:30',
-END
-
 
 --SELECT * FROM Teachers;
-SELECT * FROM Disciplines;
+--SELECT * FROM Disciplines;
--- 01-DDL/Select_Data/SQLQuery6 HomeWork.sql
+++ 01-DDL/Select_Data/SQLQuery6 HomeWork.sql
@@ -12,14 +12,20 @@ GO
 --	AND dependent_discipline=discipline_id
 
 --2)Возраст и опыт
---SELECT birth_date, work_since,
+--SELECT 
 --[Ф.И.О] = last_name + ' ' + first_name + ' ' + middle_name,
+--[Дата рождения]=birth_date,
 --[Возраст] = DATEDIFF(DAY, birth_date, GETDATE()) / 365,
+
+--[Дата устройства]=work_since,
 --[Опыт работы] = FORMATMESSAGE('%i %s', DATEDIFF(DAY, work_since, GETDATE()) / 365, 'Год')
 --FROM Teachers
+--ORDER BY [Возраст] DESC
+
 --3)Стаж больше 10 лет
 --SELECT work_since,
 --[Ф.И.О] = last_name + ' ' + first_name + ' ' + middle_name,
 --[Опыт работы] = FORMATMESSAGE('%i %s', DATEDIFF(DAY, work_since, GETDATE()) / 365, 'лет')
 --FROM Teachers
 --WHERE DATEDIFF(DAY, work_since, GETDATE()) / 365 > 10
+--ORDER BY [Опыт работы] DESC
